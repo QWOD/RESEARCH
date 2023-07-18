a347cc9ac6c376a8ca41ec53ac37723b5c5375dd
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <138945726+QWOD@users.noreply.github.com>
2023-07-17 21:42:23 -0500

    Update [[ CΔSE-ID: 0xe806caa7: ]]: $(passworder -short): fruit-generator.sh

--- [[ CΔSE-ID: 0xe806caa7: ]]: $(passworder -short): fruit-generator.sh
+++ [[ CΔSE-ID: 0xe806caa7: ]]: $(passworder -short): fruit-generator.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID: 0xe806caa7: FIG: ${FIG}:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/[[ CΔSE-ID: 0xe806caa7: ]]: ${FIG}.png)"
+export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID: 0xe806caa7: FIG: ${FIG}:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/[[%20C%CE%94SE-ID:%200xe806caa7:%20]]:%20${FIG}.png)"
