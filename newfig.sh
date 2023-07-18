0e396a6eec8f6df751dec329d2a6c1426854e7e0
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <138945726+QWOD@users.noreply.github.com>
2023-07-18 15:20:44 -0500

    Update newfig.sh

--- newfig.sh
+++ newfig.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # passworder=https://github.com/hypercasey/passworder
-export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID:0x9f82977e:${FIG}:.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/%3ACASE-ID%3A0x9f82977e%3A${FIG}%3A.png)"
-echo ":CASE-ID:0x9f82977e:${FIG}:.png"
+export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0x9f82977e-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x9f82977e-${FIG}.png)"
+echo "FIG: CASE-ID-0x9f82977e-${FIG}.png"
 echo -e "" && unset FIG
-export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID:0xe806caa7:${FIG}:.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/%3ACASE-ID%3A0xe806caa7%3A${FIG}%3A.png)"
-echo ":CASE-ID:0xe806caa7:${FIG}:.png"
+export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0xe806caa7-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0xe806caa7-${FIG}.png)"
+echo "FIG: CASE-ID-0xe806caa7-${FIG}.png"
