850495888345d7583c80c1ad5fea4f55e8c87ca0
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <138945726+QWOD@users.noreply.github.com>
2023-07-18 15:21:36 -0500

    Update newfig.sh

--- newfig.sh
+++ newfig.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# passworder=https://github.com/hypercasey/passworder
+# [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0x9f82977e-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x9f82977e-${FIG}.png)"
 echo "FIG: CASE-ID-0x9f82977e-${FIG}.png"
 echo -e "" && unset FIG
