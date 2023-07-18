13b7e9d3377b9a8faff360b1050f44ec85170086
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <138945726+QWOD@users.noreply.github.com>
2023-07-18 05:11:57 -0500

    Update and rename [[ CΔSE-ID: 0xe806caa7: ]]: $(passworder -short): fruit-generator.sh to :CΔSE-ID:0xe806caa7:$(passworder -short):.png newfig.sh

---
+++ :CΔSE-ID:0xe806caa7:$(passworder -short):.png newfig.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID:0x9f82977e:${FIG}:.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/%3ACASE-ID%3A0x9f82977e%3A${FIG}%3A.png)"
+echo ":CASE-ID:0x9f82977e:${FIG}:.png"
+echo -e "" && unset FIG
+export FIG=$(passworder -short) && echo '!'"[:CΔSE-ID:0xe806caa7:${FIG}:.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/%3ACASE-ID%3A0xe806caa7%3A${FIG}%3A.png)"
+echo ":CASE-ID:0xe806caa7:${FIG}:.png"
--- [[ CΔSE-ID: 0xe806caa7: ]]: $(passworder -short): fruit-generator.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export FIG=$(passworder -short)
-echo '!'"[:CΔSE-ID: 0xe806caa7: FIG: ${FIG}:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/[[%20C%CE%94SE-ID:%200xe806caa7:%20]]:%20${FIG}.png)"
-echo "[[ CΔSE-ID: 0xe806caa7: ]]: ${FIG}.png"
