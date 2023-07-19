6b92cb5c78d95e0efb55f07a3177e72289669232
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <138945726+QWOD@users.noreply.github.com>
2023-07-18 21:00:37 -0500

    Update newfig.sh

--- newfig.sh
+++ newfig.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
-export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0x9f82977e-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x9f82977e-${FIG}.png)"
-echo "FIG: CASE-ID-0x9f82977e-${FIG}.png"
-echo -e "" && unset FIG
-export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0xe806caa7-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0xe806caa7-${FIG}.png)"
-echo "FIG: CASE-ID-0xe806caa7-${FIG}.png"
+if [[ $* != "-p" ]]; then
+  export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0x9f82977e-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x9f82977e-${FIG}.png)"
+  echo "FIG: CASE-ID-0x9f82977e-${FIG}.png"
+  echo -e "" && unset FIG
+fi
+if [[ $* != "-p" ]]; then
+  export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0xe806caa7-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0xe806caa7-${FIG}.png)"
+  echo "FIG: CASE-ID-0xe806caa7-${FIG}.png"
+fi
+if [[ $* == "-p" ]]; then
+  for fig in `ls`; do
+    git add $fig
+  done
+  git commit -m $(passworder -uuid)
+  git push
+fi
