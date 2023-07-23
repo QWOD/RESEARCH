7a3af83858ab560aabef1e52b3f3c7fbffe9043c
QWOD-MJ12 <qwod@mj12.agency>
2023-07-23 15:43:02 -0500

    ed4fa989-3120-a887-f8a7-956087f30568

--- newfig.sh
+++ newfig.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 if [[ $* != "-p" ]]; then
-  export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0x9f82977e-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x9f82977e-${FIG}.png)"
-  echo "FIG: CASE-ID-0x9f82977e-${FIG}.png"
+  export FIG=$(passworder -short) && echo '!'"[:CASE-ID-$*-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-$*-${FIG}.png)"
+  echo "FIG: CASE-ID-$*-${FIG}.png"
   echo -e "" && unset FIG
-fi
-if [[ $* != "-p" ]]; then
-  export FIG=$(passworder -short) && echo '!'"[:CASE-ID-0xe806caa7-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0xe806caa7-${FIG}.png)"
-  echo "FIG: CASE-ID-0xe806caa7-${FIG}.png"
 fi
 if [[ $* == "-p" ]]; then
   git config --global pull.rebase false
