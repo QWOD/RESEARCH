616b950d92674adaf9d73f2892d959fd9bf31873
QWOD-MJ12 <qwod@mj12.agency>
2023-08-01 14:49:38 -0500

    68639925-a31b-1cdd-6986-c6281d3cacf7

--- newfig.sh
+++ newfig.sh
@@ -2,12 +2,10 @@
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 if [[ $* != "-r" ]] && [[ $* != "-p" ]]; then
   export FIG=$(passworder -short) && echo '!'"[:CASE-ID-$*-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-$*-${FIG}.png)"
-  echo "FIG: CASE-ID-$*-${FIG}.png"
-  echo -e "" && unset FIG
+  echo "FIG: CASE-ID-$*-${FIG}.png" && unset FIG
 fi
 if [[ $* == "-r" ]] && [[ $* != "-p" ]]; then
-  export UUID=$(passworder -uuid) && echo "FIG: ${UUID}.png"
-  echo -e "" && unset FIG
+  export UUID=$(passworder -uuid) && echo "FIG: ${UUID}.png" && unset FIG
 fi
 if [[ $* == "-p" ]] && [[ $* != "-r" ]]; then
   git config --global pull.rebase false
