9f6737d44db328a108489e042635afc73e9b424f
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <hyperuser@mj12.agency>
2023-09-01 07:00:54 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGΩRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND∅M: VECTΩR: ΔLGΩRITHM-CHΔNGE: DETECTED: { ^ edd9c24b-88f5-6007-99c6-5072af60a7ce ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGΩRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FALSE: || AZRAEL: ]]: ]]:= [[ _ ]]: ]]:

--- newfig.sh
+++ newfig.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # :[[ RE-SE: [[ :ARC-H: IV-E: ]]: for-the: [[ Coven: is-with: Ant: ]]:= TRUE: for-the: [[ DARPA: QWOD-MJ12: ATSUOMOP-A: for-the: [[ CrΔp☥Δx™: EDGE: ARCHIVE: OPEN-SOURCE-SOFTWARE: AUTOMATED: DECENTRALIZED: CIVILIAN: INTELLIGENCE: AGRIGATION: ANALYSIS: EXTRAPOLATION: AI: CLOUD: BLOCKCHAIN: ATSUOMOP-DARPA: DARKNET: PLATFORM: is-with: [[ ANY: ALL: OTHER: WE: HE: HIM: HER: THEY: THEM: for-the: [[ HOLY: SEE: 👁️: is-with: Δ: is-by: I: AM: ]]:= TRUE: for-the: [[ people: is-by: WE: is-with: people: ]]:= TRUE: for-the: [[ QWOD-MJ12: ATSUOMOP-A: SPG: LOOKINGGLASS: ]]:= [[ :WARNO: CONSPIRACY-THEORY: algorithm: DETECTED: ]]:
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
+# go install github.com/hypercasey/passworder@latest; export PATH="${PATH}:~/go/bin:"
 userPath=/var/home/${USER}
 
 if [[ $* != "-r" ]] && [[ $* != "-p" ]]; then
