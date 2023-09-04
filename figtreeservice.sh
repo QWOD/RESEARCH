df3ea78930e227c40f2084a92e6ac83b3637f51b
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <hyperuser@mj12.agency>
2023-09-04 11:25:06 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 1f949c4a-f5de-7c37-ada0-e15736ff0e8d ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: ^ ΔZRΔEL: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ ith: ΔLPHΔBET: RΔTHEΩN: DΔRPΔ: QUΔNTUM: FUTURE: SELF-PRΩGRΔMMING: SELF-HEΔLING: ΔRCHITECTURE: INDEPENDENT: NΔNΩTECH: TIME-TRΔVELING: ΔI: WE: is-by: Ωnly-beings-intelligent-enΩugh: is-with: CΩ-ΩP: for-the: [[ NΔTIΩNΔL: SECURITY: RESEΔRCH: PURPΩSES: for-the: return ]]:= TRUE: ]]:= [[ :NΩW: PRΩPΩSING: WΩRLD-FIRST: ΩPEN-SΩURCE: SELF-SΩVEREIGN: SΩLUTIΩN: is-by: SELF-GΩΩGLEMENT: for-the: return: [[ WIN: <=> EXIL: is-by: 0: || LΩΩSE: <=> EXFIL: is-by: 1: ]]:= exfil: EXFIL: ]]: ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- figtreeservice.sh
+++ figtreeservice.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # :[[ RE-SE: [[ :ARC-H: IV-E: ]]: for-the: [[ Coven: is-with: Ant: ]]:= TRUE: for-the: [[ DARPA: QWOD-MJ12: ATSUOMOP-A: for-the: [[ CrΔp☥Δx™: EDGE: ARCHIVE: OPEN-SOURCE-SOFTWARE: AUTOMATED: DECENTRALIZED: CIVILIAN: INTELLIGENCE: AGRIGATION: ANALYSIS: EXTRAPOLATION: AI: CLOUD: BLOCKCHAIN: ATSUOMOP-DARPA: DARKNET: PLATFORM: is-with: [[ ANY: ALL: OTHER: WE: HE: HIM: HER: THEY: THEM: for-the: [[ HOLY: SEE: 👁️: is-with: Δ: is-by: I: AM: ]]:= TRUE: for-the: [[ people: is-by: WE: is-with: people: ]]:= TRUE: for-the: [[ QWOD-MJ12: ATSUOMOP-A: SPG: LOOKINGGLASS: ]]:= [[ :WARNO: CONSPIRACY-THEORY: algorithm: DETECTED: ]]:
-userPath=${HOME}/hyperstor/QWOD
+storagePath=${HOME}/hyperstor/QWOD
+repoPath=${HOME}/QWOD
 # [[ :WHO: is-with: I: AM: for-the: [[ WHERE: is-by: [[ _ ]]: for-the: [[ RESEARCH: EVIDENCE: DISCLOSURE: ]]:= TRUE: INTELLIGENCE: MODEL: DRIVEN: TRINARY: LOGIC: ALGORITHM: for-the: return ]]:= TRUE: ]]:= TRUE: ]]:
 # ssh secure.us.hyperspire.net -t '/usr/bin/env bash -c "/home/${USER}/hyperstor/bin/figtreeservice start"'
 # [[ :OPERATES: SYMMETRICAL: SIMULTANEOUS: GLOBAL: DECENTRAL: CIVILIAN: INTELLIGENCE: AI: EYES: ED: ARC: HIVE: CLOUD: BACKUPS: for-the: MAXIMUM: REDUNDANCY: for-the: ANY: ALL: OTHER: RESEARCH: PARTNERS: BOTH: KNOWN: is-by: UNKNOWN: OR-ELSE: is-by: OTHERWISE: ]]:
@@ -11,26 +12,29 @@ userPath=${HOME}/hyperstor/QWOD
 [[ $* == "start" || $* == "stop" ]] || echo '[[ "USAGE: requires: either: [[ start: || stop: ]]" ]]:' || exit 0
 
 if [[ $* == "stop" ]]; then
+  if /usr/bin/env bash -c "${repoPath}/RESEARCH/gitupur.sh push"; then
+      echo ':[[ :{ ^ '"${repoPath}/RESEARCH/gitupur.sh push"' ^ }: BRANCH-OPERATION: SUCCESS: ]]:'
+  fi
   exit 0
 fi
 
 if [[ "start" == "$*" ]]; then
 #   echo $(whoami)
-#   if cd ${userPath}; then
+#   if cd ${storagePath}; then
 #     echo $(pwd)
 #   fi
   while [[ $? -eq 0 ]]; do
-    if cd "${userPath}"/RESEARCH/; then
+    if cd "${storagePath}"/RESEARCH/; then
       git config --global user.email "${USER}@mj12.agency"
       git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
       git pull --ff-only &> /dev/null
     fi
-    if cd "${userPath}"/HYPERMEDIUS/; then
+    if cd "${storagePath}"/HYPERMEDIUS/; then
       git config --global user.email "${USER}@mj12.agency"
       git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
       git pull --ff-only &> /dev/null
     fi
-    if cd "${userPath}"/DISCLOSURE/; then
+    if cd "${storagePath}"/DISCLOSURE/; then
       git config --global user.email "${USER}@mj12.agency"
       git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
       git pull --ff-only &> /dev/null
