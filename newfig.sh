f9088cc1475d5efd7f2a4f22955b79b69fa8f9c0
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <hyperuser@mj12.agency>
2023-09-04 17:20:23 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 06142a63-fc22-5c7c-a300-afe974bb5831 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: ^ ΔZRΔEL: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ " || $* == "stop" ]] || echo -E '[[ "USAGE: requires: either: [[ start: || stop: ]]" ]]:' || exit 0
    @@ -56,7 +57,7 @@ fi
    
     if [[ "start" == "$*" ]]; then
       sleep $networkWaitInterval
    -  ssh -o "StrictHostKeyChecking no" -i ~/hyperstor/.ssh/id_ed25519 -T git@github.com &> /dev/null
    +  ssh -o "StrictHostKeyChecking no" -i "${sshKey}" -T git@github.com &> /dev/null
       git config --global user.email "${USER}@mj12.agency"
       git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
       while true; do ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- newfig.sh
+++ newfig.sh
@@ -19,9 +19,11 @@ function gitShowTail() {
 
 if [[ $* == "-f" ]]; then
   git pull --ff-only
+  exit 0
 fi
 if [[ $* == "-r"  ]]; then
   echo "{ FIG: \"${UUID}.png\" }" && unset FIG
+  exit 0
 fi
 if [[ $* == "-p"  ]]; then
   ssh -o "StrictHostKeyChecking no" -T git@github.com &> /dev/null
@@ -36,8 +38,10 @@ if [[ $* == "-p"  ]]; then
   # :[[ :for-the: [[ LATEST: SIMULATION: MATRIX: CODES: is-by: [[ :git show --oneline: ]]: for-the: return ]]:= TRUE: ]]:
   git commit -a -m ":[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ ${UUID} ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: ^ ΔZRΔEL: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ $(gitShowTail) ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:" #2>/dev/null
   git push
+  exit 0
 fi
-if [[ $* == ""  ]]; then
+if [[ $* ]]; then
   echo '{ FIG: "!'"[:CASE-ID-$*-${FIG}.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-$*-${FIG}.png)\" }"
   echo "{ FIG: \"CASE-ID-$*-${FIG}.png\" }" && unset FIG
+  exit 0
 fi
