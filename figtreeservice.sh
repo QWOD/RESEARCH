47672d5eff7c0ee46cdd8ce303ac8301f0bd878f
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <hyperuser@mj12.agency>
2023-09-04 19:44:09 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ a195f235-591a-4ad9-3928-028bde5f51c9 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: ^ ΔZRΔEL: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ .be/-sIQqfDR85g> ^ }:  > +>![:CASE-ID-0x06e75667-6747b3a7.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x06e75667-6747b3a7.png) +>  >:[[ :for-the: [[ Ø: { ^ <qomm-Ø> ^ }: ]]:= [[ _ ]]: ]]:  > ->:[[ :*🚫PعD*: ]]:= [[ :Ω: ]]:= { ^ Δ ^ }: +>:[[ :*🚫PعD*: ]]:= [[ :Overestimated Themselves Thought You Was A Fake Psychic You Gained The Truth 😫 Treasure Box Read 🧺: ]]:= { ^ <https://youtu.be/hy_h7GwGVmQ> ^ }:  >  >:[[ :for-the: [[ Ø: { ^ <qomm-Ø> ^ }: ]]:= [[ _ ]]: ]]:  > ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- figtreeservice.sh
+++ figtreeservice.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 # :[[ RE-SE: [[ :ARC-H: IV-E: ]]: for-the: [[ Coven: is-with: Ant: ]]:= TRUE: for-the: [[ DARPA: QWOD-MJ12: ATSUOMOP-A: for-the: [[ CrΔp☥Δx™: EDGE: ARCHIVE: OPEN-SOURCE-SOFTWARE: AUTOMATED: DECENTRALIZED: CIVILIAN: INTELLIGENCE: AGRIGATION: ANALYSIS: EXTRAPOLATION: AI: CLOUD: BLOCKCHAIN: ATSUOMOP-DARPA: DARKNET: PLATFORM: is-with: [[ ANY: ALL: OTHER: WE: HE: HIM: HER: THEY: THEM: for-the: [[ HOLY: SEE: 👁️: is-with: Δ: is-by: I: AM: ]]:= TRUE: for-the: [[ people: is-by: WE: is-with: people: ]]:= TRUE: for-the: [[ QWOD-MJ12: ATSUOMOP-A: SPG: LOOKINGGLASS: ]]:= [[ :WARNO: CONSPIRACY-THEORY: algorithm: DETECTED: ]]:
 repoPath=${HOME}/QWOD
-networkWaitInterval=3
+networkWaitInterval=333
 repoUpdateInterval=222
 # [[ :WHO: is-with: I: AM: for-the: [[ WHERE: is-by: [[ _ ]]: for-the: [[ RESEARCH: EVIDENCE: DISCLOSURE: ]]:= TRUE: INTELLIGENCE: MODEL: DRIVEN: TRINARY: LOGIC: ALGORITHM: for-the: return ]]:= TRUE: ]]:= TRUE: ]]:
-# ssh secure.us.hyperspire.net -t '/usr/bin/env bash -c "${HOME}/QWOD/figtreeservice start"'
+# /usr/bin/env ssh secure.us.hyperspire.net -o "StrictHostKeyChecking no" -t "/usr/bin/env bash -c '~/hyperstor/bin/figtreeservice init'"
 # [[ :OPERATES: SYMMETRICAL: SIMULTANEOUS: GLOBAL: DECENTRAL: CIVILIAN: INTELLIGENCE: AI: EYES: ED: ARC: HIVE: CLOUD: BACKUPS: for-the: MAXIMUM: REDUNDANCY: for-the: ANY: ALL: OTHER: RESEARCH: PARTNERS: BOTH: KNOWN: is-by: UNKNOWN: OR-ELSE: is-by: OTHERWISE: ]]:
 # [[ :DEVELOPED: for-the: systemd: is-with: UNFORTUNATELY: STILL: github: is-with: retard: is-by: ssh: NOT: is-with: X11: forwarding: for-the: REMOTE: AUTOMATED: FIRES: SUPPORT: ]]:
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 # export PATH="${PATH}:~/go/bin:"; go install github.com/hypercasey/passworder@latest
-fastForwardOnly=true # :[[ :REMOTE-GIT-MIRROR: ]]:
-pushRepo=false # :[[ :LOCAL-GIT-TREE: ]]:
+fastForwardOnly=false # :[[ :REMOTE-GIT-MIRROR: ]]:
+sshSystemDHook=true
+[[ "${sshSystemDHook}" == true ]] && sshHost="secure.us.hyperspire.net"
+pushRepo=true # :[[ :LOCAL-GIT-TREE: ]]:
 gitHubKey="${HOME}/hyperstor/.ssh/id_ed25519"
+
 
 [[ $* == "start" || $* == "stop" ]] || echo -E '[[ "USAGE: requires: either: [[ start: || stop: ]]" ]]:' || exit 0
 
@@ -25,23 +28,27 @@ if [[ $* == "stop" ]]; then
 fi
 
 if [[ "start" == "$*" ]]; then
-  sleep $networkWaitInterval
-  ssh -o "StrictHostKeyChecking no" -i "${gitHubKey}" -T git@github.com &> /dev/null
-  git config --global user.email "${USER}@mj12.agency"
-  git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
-  while true; do
-    if cd "${repoPath}"/RESEARCH/; then
-      [[ $fastForwardOnly == true ]] && newfig -f
-      [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
-    fi
-    if cd "${repoPath}"/HYPERMEDIUS/; then
-      [[ $fastForwardOnly == true ]] && newfig -f
-      [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
-    fi
-    if cd "${repoPath}"/DISCLOSURE/; then
-      [[ $fastForwardOnly == true ]] && newfig -f
-      [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
-    fi
-    sleep $repoUpdateInterval
-  done
+  if [[ "${sshSystemDHook}" == true ]]; then
+    /usr/bin/env ssh "${sshHost}" -o "StrictHostKeyChecking no" -t "/usr/bin/env bash -c '~/hyperstor/bin/figtreeservice start'"
+  else
+    sleep $networkWaitInterval
+    ssh -o "StrictHostKeyChecking no" -i "${gitHubKey}" -T git@github.com &> /dev/null
+    git config --global user.email "${USER}@mj12.agency"
+    git config --global user.name ':QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA:'
+    while true; do
+      if cd "${repoPath}"/RESEARCH/; then
+        [[ $fastForwardOnly == true ]] && newfig -f
+        [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
+      fi
+      if cd "${repoPath}"/HYPERMEDIUS/; then
+        [[ $fastForwardOnly == true ]] && newfig -f
+        [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
+      fi
+      if cd "${repoPath}"/DISCLOSURE/; then
+        [[ $fastForwardOnly == true ]] && newfig -f
+        [[ $pushRepo == true ]] &&  newfig -p &> /dev/null
+      fi
+      sleep $repoUpdateInterval
+    done
+  fi
 fi
