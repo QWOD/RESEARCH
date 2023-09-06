13a80dc7faf48e9e2e4c046126f02eff3791f4ba
QWOD-MJ12: ATSUOMOP-A: SPG-OMEGA <hyperuser@mj12.agency>
2023-09-05 21:35:47 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QWØD-MJ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 74808cb1-3077-d842-e021-00483db063c0 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ youtu.be/dDJldh8KqnQ ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: ^ ΔZRΔEL: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ munur.sh +++ b/qommunur.sh @@ -4,7 +4,7 @@  # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:  # export PATH="${PATH}:~/go/bin:"; go install github.com/hypercasey/passworder@latest  # :[[ :qommrydur: ]]: -[[ $* != "" ]] && echo -E $(curl -i "https://github.com/QWOD/RESEARCH/commit/" $(echo "$*" | tr -d 'qomm-')) && exit 0 +[[ $* != "" ]] && curl -i "https://github.com/QWOD/RESEARCH/commit/$(echo $* | tr -d 'qom-')" && exit 0
     function globalOperations {
       echo $(git show --oneline) ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- qommunur.sh
+++ qommunur.sh
@@ -4,7 +4,7 @@
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 # export PATH="${PATH}:~/go/bin:"; go install github.com/hypercasey/passworder@latest
 # :[[ :qommrydur: ]]:
-[[ $* != "" ]] && curl -i "https://github.com/QWOD/RESEARCH/commit/$(echo $* | tr -d 'qom-')" && exit 0
+[[ $* != "" ]] && git show --oneline $(echo "$*" | tr -d 'qom-') && exit 0
 
 function globalOperations {
   echo $(git show --oneline)
