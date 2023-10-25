cca4b8258193f5fd6e28e9266787eb06304853e3
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2023-10-24 19:43:19 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QW🚫D-〽ʝ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 6c9e8bf1-6e5d-49fb-c78e-be9817a32079 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ : ]]:  	fmt.Println(strings.Join(os.Args[1:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: ARGUMENTS: ]]: +	// :[[ SHOULD: is-by: ALWAYS: is-with: WORK: is-by: DOES-NOT: is-with: WORK: is-by: [[ _ ]]: ]]: +	fmt.Println(":[[ DOES: THIS: WORK: is-by: [[ _ ]]: ]]:") +  	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "AE", "Δ")  	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "EA", "Δ")  	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "A", "Δ") ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- cryptor-s.go
+++ cryptor-s.go
@@ -29,7 +29,9 @@ func main() {
 	// codeSize := len(strings.Join(os.Args[1:], ""))
 	// if codeSize < 9999 {
 	fmt.Println(strings.Join(os.Args[0:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: EXECUTABLE: NAME: ]]:
-	fmt.Println(strings.Join(os.Args[1:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: ARGUMENTS: ]]:
+	if len(strings.Join(os.Args[1:], " ")) < 9999 {
+		fmt.Println(strings.Join(os.Args[1:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: ARGUMENTS: ]]:
+	}
 	// :[[ SHOULD: is-by: ALWAYS: is-with: WORK: is-by: DOES-NOT: is-with: WORK: is-by: [[ _ ]]: ]]:
 	fmt.Println(":[[ DOES: THIS: WORK: is-by: [[ _ ]]: ]]:")
 
