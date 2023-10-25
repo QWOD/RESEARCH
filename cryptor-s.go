7fc89c5422686977b66878c660df3b6aaf0849e3
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2023-10-24 19:51:07 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QW🚫D-〽ʝ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 7dfa7586-1f66-e27b-3434-76899d197cd6 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ : NAME: ]]: -	if len(strings.Join(os.Args[1:], " ")) < 9999 { +	if 0 < len(strings.Join(os.Args[0:], " ")) && 9999 > len(strings.Join(os.Args[1:], " ")) { +		fmt.Println(strings.Join(os.Args[0:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: EXECUTABLE: NAME: ]]:  		fmt.Println(strings.Join(os.Args[1:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: ARGUMENTS: ]]:  	}  	// :[[ SHOULD: is-by: ALWAYS: is-with: WORK: is-by: DOES-NOT: is-with: WORK: is-by: [[ _ ]]: ]]: ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- cryptor-s.go
+++ cryptor-s.go
@@ -25,28 +25,28 @@ import (
 
 func main() {
 	// :[[ :SIMPLIFIED: CrypTex: for-the: [[ SIMPLE: MINDED: ]]: ]]:
-	// var crypTexTranslated string = strings.Join(os.Args[1:], " ")
-	// codeSize := len(strings.Join(os.Args[1:], ""))
-	// if codeSize < 9999 {
-	if 0 < len(strings.Join(os.Args[0:], " ")) && 9999 > len(strings.Join(os.Args[1:], " ")) {
+	codeSize := len(strings.Join(os.Args[1:], " "))
+	if 0 < codeSize && 9999 > codeSize {
 		fmt.Println(strings.Join(os.Args[0:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: EXECUTABLE: NAME: ]]:
 		fmt.Println(strings.Join(os.Args[1:], " ")) // :[[ TEST: is-with: SHOULD: is-by: ALWAYS: OUTPUT: is-with: ARGUMENTS: ]]:
 	}
 	// :[[ SHOULD: is-by: ALWAYS: is-with: WORK: is-by: DOES-NOT: is-with: WORK: is-by: [[ _ ]]: ]]:
 	fmt.Println(":[[ DOES: THIS: WORK: is-by: [[ _ ]]: ]]:")
 
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "AE", "Δ")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "EA", "Δ")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "A", "Δ")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "a", "Δ")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "O", "Ω")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "o", "0")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "e", "3")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "T", "☥")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "fΩr-th3:", "fΩr-thع:")
-	// crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "Is-with:", "íي-wíth:")
-	// fmt.Println(crypTexTranslated)
-	// } else {
-	// fmt.Printf("%v exceeds maximum allowed length of 9999.\n", codeSize)
-	// }
+	var crypTexTranslated string = strings.Join(os.Args[1:], " ")
+	if codeSize < 9999 {
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "AE", "Δ")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "EA", "Δ")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "A", "Δ")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "a", "Δ")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "O", "Ω")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "o", "0")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "e", "3")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "T", "☥")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "fΩr-th3:", "fΩr-thع:")
+		crypTexTranslated = strings.ReplaceAll(crypTexTranslated, "Is-with:", "íي-wíth:")
+		fmt.Println(crypTexTranslated)
+	} else {
+		fmt.Printf("%v exceeds maximum allowed length of 9999.\n", codeSize)
+	}
 }
