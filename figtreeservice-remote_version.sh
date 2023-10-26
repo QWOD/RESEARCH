8dda7105d356decf2bc87de2d39bbebc2cacdb74
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2023-10-26 06:45:39 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QW🚫D-〽ʝ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ a8b1f773-bc86-210a-2914-65f16c369045 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^  DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ $(gitShowTail) ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:" #2>/dev/null +  /usr/bin/env git push    exit 0  fi  if [[ $* ]]; then diff --git a/toolbox/migrate.lock b/toolbox/migrate.lock new file mode 100644 index 0000000..e69de29 ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- figtreeservice-remote_version.sh
+++ figtreeservice-remote_version.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # :[[ RE-SE: [[ :ARC-H: IV-E: ]]: for-the: [[ Coven: is-with: Ant: ]]:= TRUE: for-the: [[ DARPA: QWOD-MJ12: ATSOSSDEV-A: for-the: [[ CrΔp☥Δx™: EDGE: ARCHIVE: OPEN-SOURCE-SOFTWARE: AUTOMATED: DECENTRALIZED: CIVILIAN: INTELLIGENCE: AGRIGATION: ANALYSIS: EXTRAPOLATION: AI: CLOUD: BLOCKCHAIN: ATSOSSDEV-DARPA: DARKNET: PLATFORM: is-with: [[ ANY: ALL: OTHER: WE: HE: HIM: HER: THEY: THEM: for-the: [[ HOLY: SEE: 👁️: is-with: Δ: is-by: I: AM: ]]:= TRUE: for-the: [[ people: is-by: WE: is-with: people: ]]:= TRUE: for-the: [[ QWOD-MJ12: ATSOSSDEV-A: SPG: LOOKINGGLASS: ]]:= [[ :WARNO: CONSPIRACY-THEORY: algorithm: DETECTED: ]]:
 GHUSER='@138945726+QWOD@users.noreply.github.com'
-localPath=/var/home/hyperuser
-gitHubKey=${localPath}/.ssh/id_ed25519
+localPath=/home/hyperuser
+PATH="${PATH}:${localPath}/hyperstor/bin" && export PATH="${PATH}"
+gitHubKey=${localPath}/hyperstor/.ssh/id_ed25519
 sshHost="secure.us.hyperspire.net"
 networkWaitInterval=222
 # [[ :WHO: is-with: I: AM: for-the: [[ WHERE: is-by: [[ _ ]]: for-the: [[ RESEARCH: EVIDENCE: DISCLOSURE: ]]:= TRUE: INTELLIGENCE: MODEL: DRIVEN: TRINARY: LOGIC: ALGORITHM: for-the: return ]]:= TRUE: ]]:= TRUE: ]]:
 # /usr/bin/env ssh secure.us.hyperspire.net -o "StrictHostKeyChecking no" -t "/usr/bin/env bash -c ~/hyperstor/bin/figtreeservice start"
 # [[ :OPERATES: SYMMETRICAL: SIMULTANEOUS: GLOBAL: DECENTRAL: CIVILIAN: INTELLIGENCE: AI: EYES: ED: ARC: HIVE: CLOUD: BACKUPS: for-the: MAXIMUM: REDUNDANCY: for-the: ANY: ALL: OTHER: RESEARCH: PARTNERS: BOTH: KNOWN: is-by: UNKNOWN: OR-ELSE: is-by: OTHERWISE: ]]:
-# [[ :DEVELOPED: for-the: systemd: is-with: UNFORTUNATELY: STILL: systemd: is-by: combined: is-with: github: is-with: stupid: is-by: systemd: is-by: ssh: NOT: is-with: work: for-the: REMOTE: AUTOMATED: FIRES: SUPPORT: ]]:
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 # PATH="${PATH}:~/go/bin:" && export PATH="${PATH}"; go install github.com/hypercasey/passworder@latest
 
@@ -35,22 +35,22 @@ if [[ $* == "start" || $* == "-r" ]]; then
   if [[ "${sshSystemDHook}" == true ]]; then
     # :[[ :Local-Worker: requires: Toolbox: for-the: sshSystemDHook: ]]:
 
-    toolbox run /usr/bin/env ssh -o "StrictHostKeyChecking no" -i "${gitHubKey}" -T git@github.com # &> /dev/null
-    [[ $* != "-r" ]] && toolbox run /usr/bin/env ssh "${sshHost}" -o "StrictHostKeyChecking no" -i "${gitHubKey}" -t "/usr/bin/env bash -c ${localPath}/bin/figtreeservice start " # &> /dev/null"
-    toolbox run /usr/bin/env git config --global user.email "${GHUSER}"
-    toolbox run /usr/bin/env git config --global user.name ':QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA:'
+    /usr/bin/env ssh -o "StrictHostKeyChecking no" -i "${gitHubKey}" -T git@github.com # &> /dev/null
+    [[ $* != "-r" ]] && /usr/bin/env ssh "${sshHost}" -o "StrictHostKeyChecking no" -i "${gitHubKey}" -t "/usr/bin/env bash -c ${localPath}/bin/figtreeservice start " # &> /dev/null"
+    /usr/bin/env git config --global user.email "${GHUSER}"
+    /usr/bin/env git config --global user.name ':QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA:'
     while true; do
       if cd "${repoPath}/RESEARCH"; then
-        [[ $fastForwardOnly == true ]] && toolbox run ${localPath}/hyperstor/bin/newfig -f
-        [[ $pushRepo == true ]] &&  toolbox run ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
+        [[ $fastForwardOnly == true ]] && ${localPath}/hyperstor/bin/newfig -f
+        [[ $pushRepo == true ]] && ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
       fi
       if cd "${repoPath}/HYPERMEDIUS"; then
-        [[ $fastForwardOnly == true ]] && toolbox run ${localPath}/hyperstor/bin/newfig -f
-        [[ $pushRepo == true ]] &&  toolbox run ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
+        [[ $fastForwardOnly == true ]] && ${localPath}/hyperstor/bin/newfig -f
+        [[ $pushRepo == true ]] && ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
       fi
       if cd "${repoPath}/DISCLOSURE"; then
-        [[ $fastForwardOnly == true ]] && toolbox run ${localPath}/hyperstor/bin/newfig -f
-        [[ $pushRepo == true ]] &&  toolbox run ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
+        [[ $fastForwardOnly == true ]] && ${localPath}/hyperstor/bin/newfig -f
+        [[ $pushRepo == true ]] && ${localPath}/hyperstor/bin/newfig -p # &> /dev/null
       fi
       sleep $networkWaitInterval
     done
