164dca4ce95ee183f124a928d7d2d6afddcceaec
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2023-10-27 07:35:04 -0500

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QW🚫D-〽ʝ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 3a60aec6-355d-9fc7-1b35-d1bc5b2f9002 ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^  [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]: diff --git a/CASE-ID-0x6862071a.ctxt.md b/CASE-ID-0x6862071a.ctxt.md index 0bc62dd..e952eef 100644 --- a/CASE-ID-0x6862071a.ctxt.md +++ b/CASE-ID-0x6862071a.ctxt.md @@ -561,7 +561,7 @@
     :[[ :for-the: [[ Ø: { ^ <qomm-Ø> ^ }: ]]:= { [[ _ ]]: "[[ _ ]]" }: ]]:
    
    ->
    +>![:CASE-ID-0x6862071a-50b2072e.png:](https://raw.githubusercontent.com/QWOD/HYPERMEDIUS/main/CASE-ID-0x6862071a-50b2072e.png)
     >
     >## *[[ :Atlas ATV – The Most Capable Off-Road Vehicle Ever?: ]]*
     > ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- gitupur.sh
+++ gitupur.sh
@@ -28,8 +28,8 @@ function cloudHiveBackup {
     "cd ${gitTreePath}/QWOD; /usr/bin/env git pull --no-rebase --ff-only git@github.com:QWOD/DISCLOSURE.git"
   )
   [[ "push" == "${specialWarfareTask}" ]] && specialOperations=(
-    "toolbox run /usr/bin/env ssh -o 'StrictHostKeyChecking no' -T git@github.com &> /dev/null; /usr/bin/env git config --global user.email ${GHUSER}; /usr/bin/env git config --global user.name ':QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA:'"
-    "toolbox run cd ~/QWOD/HYPERMEDIUS && ~/bin/newfig -p; cd ~/QWOD/RESEARCH && ~/bin/newfig -p; cd ~/QWOD/DISCLOSURE && ~/bin/newfig -p"
+    "toolbox run \"/usr/bin/env ssh -o 'StrictHostKeyChecking no' -T git@github.com &> /dev/null; /usr/bin/env git config --global user.email ${GHUSER}; /usr/bin/env git config --global user.name ':QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA:'; cd ~/QWOD/HYPERMEDIUS && ~/bin/newfig -p; cd ~/QWOD/RESEARCH && ~/bin/newfig -p; cd ~/QWOD/DISCLOSURE && ~/bin/newfig -p\""
+  )
   )
    
   for sop in "${specialOperations[@]}"; do
