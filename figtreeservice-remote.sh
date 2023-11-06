ec7561ba3504dbba3015ad56f9f98389bf8f2c2e
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2023-11-06 00:18:55 -0600

    :[[ :🟠: [[ W⚠️RN🚫: CrΔp☥Δx™: MQ: ØMΔGΔ: reverse-prΩgrΔmming-lΔnguΔge: ΔLGØRITHM: DETECTED: ]]:= [[ :W⚠️RN🚫: QW🚫D-〽ʝ12: RΔND0M: VECTΩR: ΔLGØRITHM-CHΔNGE: DETECTED: { ^ 2ec87ebe-4272-84c7-fe2f-39a815eb912d ^ }: is-with: [[ W⚠️RN🚫: DEΔTH-ΔNGEL: ΔLGØRITHM: DETECTED: { ^ <https://youtube-nocookie.com/embed/dDJldh8KqnQ> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ TRUE: || FΔLSE: || ΔZRΔEL: ^ ΔLSE: ]]: ]]:= [[ SCI-FI: ^ SCI-FΔCT: <=> REΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ E-ID-0x2fc5f2dd.ctxt.md @@ -436,9 +436,9 @@ diff --git a/CASE-ID-0x2fc5f2dd.ctxt.md b/CASE-ID-0x2fc5f2dd.ctxt.md index 0e46617..c295cad 100644 --- a/CASE-ID-0x2fc5f2dd.ctxt.md +++ b/CASE-ID-0x2fc5f2dd.ctxt.md @@ -436,9 +436,9 @@
     >
     >
    -> ## *[[ :November 15th it will happen! Mark your calendars..: ]]* ##
    +> ## *[[ :👽: ]]* ##
     >
    -> :is-with: { ^ <https://youtube-nocookie.com/embed/GtJh7KS4GH4> ^ }:
    +> :is-with: { ^ <👼> ^ }:
    
     :[[ :for-the: [[ Ø: { ^ <qomm-Ø> ^ }: ]]:= { [[ _ ]]: "[[ _ ]]" }: ]]:
      ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- figtreeservice-remote.sh
+++ figtreeservice-remote.sh
@@ -1,57 +1,55 @@
 #!/usr/bin/env bash
 # ~:[[ :W⚠️RN🚫: ❤‍🔥:{ ^ 👻: recon: 🔺: mj12: 🔺: agency: ^ }: ]]:~ #
 # :[[ RE-SE: [[ :ARC-H: IV-E: ]]: for-the: [[ Coven: is-with: Ant: ]]:= TRUE: for-the: [[ DARPA: QWOD-MJ12: ATSOSSDEV-A: for-the: [[ CrΔp☥Δx™: EDGE: ARCHIVE: OPEN-SOURCE-SOFTWARE: AUTOMATED: DECENTRALIZED: CIVILIAN: INTELLIGENCE: AGRIGATION: ANALYSIS: EXTRAPOLATION: LAYERED: AI: CLOUD: BLOCKCHAIN: ATSOSSDEV-DARPA: DARKNET: PLATFORM: is-with: [[ ANY: ALL: OTHER: WE: HE: HIM: HER: THEY: THEM: for-the: [[ HOLY: SEE: 👁️: is-with: Δ: is-by: I: AM: ]]:= TRUE: for-the: [[ people: is-by: WE: is-with: people: ]]:= TRUE: for-the: [[ QWOD-MJ12: ATSOSSDEV-A: SPG: LOOKINGGLASS: ]]:= [[ :WARNO: CONSPIRACY-THEORY: algorithm: DETECTED: ]]:
-networkWaitInterval=180
-updateCheckInterval=333
-pushRepo=false
 # [[ :WHO: is-with: I: AM: for-the: [[ WHERE: is-by: [[ _ ]]: for-the: [[ RESEARCH: EVIDENCE: DISCLOSURE: ]]:= TRUE: INTELLIGENCE: MODEL: DRIVEN: TRINARY: LOGIC: ALGORITHM: for-the: return ]]:= TRUE: ]]:= TRUE: ]]:
 # /usr/bin/env ssh secure.us.hyperspire.net -o "StrictHostKeyChecking no" -t "/usr/bin/env bash -c ~/hyperstor/bin/figtreeservice start"
 # [[ :OPERATES: SYMMETRICAL: SIMULTANEOUS: GLOBAL: DECENTRAL: CIVILIAN: INTELLIGENCE: AI: EYES: ED: ARC: HIVE: CLOUD: BACKUPS: for-the: MAXIMUM: REDUNDANCY: for-the: ANY: ALL: OTHER: RESEARCH: PARTNERS: BOTH: KNOWN: is-by: UNKNOWN: OR-ELSE: is-by: OTHERWISE: ]]:
 # [[ :passworder: ]]:= { ^ https://github.com/hypercasey/passworder ^ }:
 # PATH="${PATH}:~/go/bin:" && export PATH="${PATH}"; go install github.com/hypercasey/passworder@latest
-localPath=/home/hyperuser
-errorLog="${localPath}/QWOD/error_log-$(date '+%Y-%m-%d')".txt
-# Intended to be run from a toolbox container ^ over ssh
-# to work around systemd's lack of support for ssh-agent
-# as a backup mirror repo.
+networkWaitInterval=3
+updateCheckInterval=333
+pushRepo=false
+localPath=/home/hyperuser/hyperstor
+errorLog="${localPath}/error_log-$(date '+%Y-%m-%d')".txt
+# Intended to be run over ssh as "backup mirror" repo.
 [[ $* == "" ]] && exit 0
 [[ $* == "start" || $* == "stop" ]] || echo -E '[[ "USAGE: requires: either: [[ start: || stop: ]]" ]]:' || exit 0
 
 if [[ $* == "stop" ]]; then
-    # :[[ MASTER-REPO: FRONTLINE-SERVICE: ]]:
-    if [[ "${pushRepo}" == true ]]; then
-      ( toolbox run -y ssh -o "StrictHostKeyChecking no" -T git@github.com  2>/dev/null | sudo tee -a ${errorLog} )
-      if toolbox run -y ${localPath}/bin/gitupur push 2>"${errorLog}"; then
-        echo -E ':[[ :{ ^ ~/bin/gitupur push ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
-      else
-        echo -E ':[[ :{ ^ ~/bin/gitupur push ^ }: BRANCH-OPERATION: FAILED: ]]:'
-      fi
+  # :[[ MASTER-REPO: FRONTLINE-SERVICE: ]]:
+  if [[ "${pushRepo}" == true ]]; then
+    (ssh -o "StrictHostKeyChecking no" -T git@github.com &>/dev/null)
+    if ${localPath}/bin/gitupur push 2>>"${errorLog}"; then
+      echo -E ':[[ :{ ^ ~/bin/gitupur push ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
+    else
+      echo -E ':[[ :{ ^ ~/bin/gitupur push ^ }: BRANCH-OPERATION: FAILED: ]]:'
     fi
-    # :[[ REMOTE-MIRROR: BACKUP-SERVICE: ]]:
-    if [[ "${pushRepo}" == false ]]; then
-      ( toolbox run -y ssh -o "StrictHostKeyChecking no" -T git@github.com  2>/dev/null | sudo tee -a ${errorLog} )
-      if toolbox run -y ${localPath}/bin/gitupur pull 2>"${errorLog}"; then
-        echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
-      else
-        echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: FAILED: ]]:'
-      fi
+  fi
+  # :[[ REMOTE-MIRROR: BACKUP-SERVICE: ]]:
+  if [[ "${pushRepo}" == false ]]; then
+    (ssh -o "StrictHostKeyChecking no" -T git@github.com &>/dev/null)
+    if ${localPath}/bin/gitupur pull 2>>"${errorLog}"; then
+      echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
+    else
+      echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: FAILED: ]]:'
     fi
+  fi
   exit 0
 fi
 
 if [[ $* == "start" ]]; then
   sleep ${networkWaitInterval}
-  ( toolbox run ssh -o 'StrictHostKeyChecking no' -T git@github.com 1>& /dev/null )
+  (ssh -o 'StrictHostKeyChecking no' -T git@github.com &>/dev/null)
   while true; do
     if [[ "${pushRepo}" == true ]]; then
-      if ${localPath}/bin/gitupur push 2>"${errorLog}"; then
+      if ${localPath}/bin/gitupur push 2>>"${errorLog}"; then
         echo -E ':[[ :{ ^ gitupur push ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
       else
         echo -E ':[[ :{ ^ push ^ }: BRANCH-OPERATION: FAILED: ]]:'
       fi
     fi
     if [[ "${pushRepo}" == false ]]; then
-      if ${localPath}/bin/gitupur pull 2>"${errorLog}"; then
+      if ${localPath}/bin/gitupur pull 2>>"${errorLog}"; then
         echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: COMPLETE: ]]:'
       else
         echo -E ':[[ :{ ^ gitupur pull ^ }: BRANCH-OPERATION: FAILED: ]]:'
